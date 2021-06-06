c18f995c6de306d7950eb02aa8a22f1c9c7756f7
Margaret <match14@yandex.ru>
2021-06-06 16:29:23 +0300

    header

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,23 @@
 import {$} from './common';
 
-$(window).scroll(function(){
-	if($(this).scrollTop()>300){
-		$('.js-move-up').addClass('visible');
-	}else{
-		$('.js-move-up').removeClass('visible');
-	}
-});
-$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+// $(window).scroll(function(){
+// 	if($(this).scrollTop()>300){
+// 		$('.js-move-up').addClass('visible');
+// 	}else{
+// 		$('.js-move-up').removeClass('visible');
+// 	}
+// });
+// $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+
+// show/hide form search
+if($('.js-search-form').length){
+	$('.js-search-form-btn').on('click', function() {
+		$(this).closest('.js-search-form').addClass('active');
+	});
+
+	$(document).on('click', function(event) {
+		if ($(event.target).closest(".js-search-form").length) return;
+		$('.js-search-form').removeClass('active');
+		event.stopPropagation();
+	});
+}
