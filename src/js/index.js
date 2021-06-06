a3a8beb87eace4f8cfa4fe213f23a3308942f6a4
Margaret <match14@yandex.ru>
2021-06-06 21:33:38 +0300

    footer + popup

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,14 @@
 import {$} from './common';
 
-// $(window).scroll(function(){
-// 	if($(this).scrollTop()>300){
-// 		$('.js-move-up').addClass('visible');
-// 	}else{
-// 		$('.js-move-up').removeClass('visible');
-// 	}
-// });
-// $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+// move-up
+$(window).on('scroll', function(){
+	if($(this).scrollTop()>300){
+		$('.js-move-up').addClass('visible');
+	}else{
+		$('.js-move-up').removeClass('visible');
+	}
+});
+$('.js-move-up').on('click', function(){$('body,html').animate({scrollTop:0},800);return false;});
 
 // show/hide form search
 if($('.js-search-form').length){
