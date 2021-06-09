6bb0bccec4dda910e6f16068abd6b2c414e07b77
Margaret <match14@yandex.ru>
2021-06-10 00:12:06 +0300

    part main page

--- src/js/index.js
+++ src/js/index.js
@@ -15,10 +15,20 @@ if($('.js-search-form').length){
 	$('.js-search-form-btn').on('click', function() {
 		$(this).closest('.js-search-form').addClass('active');
 	});
-
+	
 	$(document).on('click', function(event) {
 		if ($(event.target).closest(".js-search-form").length) return;
 		$('.js-search-form').removeClass('active');
 		event.stopPropagation();
+	});
+}
+
+// top slider
+if($('.js-top-slider').length){
+	$('.js-top-slider-list').slick({
+		infinite: true,
+		vertical: true,
+		prevArrow: $(".js-btn-arr-up"),
+		nextArrow: $(".js-btn-arr-down"),
 	});
 }
